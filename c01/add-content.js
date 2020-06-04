500be7b44bdb5f6b2cf1656c999be6d746a92169
Nathan Kinkade <nkkinkade@gmail.com>
2020-06-03 21:53:18 -0500

    add prompts from Monty Python

--- c01/add-content.js
+++ c01/add-content.js
@@ -1,15 +1,18 @@
 var today = new Date();
 var hourNow = today.getHours();
+var userName = prompt('What is your name?');
+var quest = prompt('What is your quest?');
+var swallow = prompt('What is the air-speed velocity of an unladen swallow?');
 var greeting;
 
 if (hourNow > 18) {
-    greeting = 'Good evening!';
+    greeting = 'Good evening, ' + userName + '!';
 } else if (hourNow > 12) {
-    greeting = 'Good afternoon!';
+    greeting = 'Good afternoon, ' + userName + '!';
 } else if (hourNow > 0) {
-    greeting = 'Good morning!';
+    greeting = 'Good morning, ' + userName + '!';
 } else {
-    greeting = 'Welcome!'
+    greeting = 'Welcome, ' + userName + '!'
 }
 
 document.write('<h3>' + greeting + '</h3>');
